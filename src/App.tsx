6e9d43384c8ccace66ab34da41ea811b7c3df633
Vinicius <wh0adscrypt@gmail.com>
2020-01-24 12:40:23 -0300

    feat: create a example TS with Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,77 @@
-import React from 'react'
-import './App.css'
+import React, { Fragment, useContext, useEffect } from 'react'
+import { Store } from './Store'
+import { IAction, IEpisode } from './interfaces'
 
-function App() {
+export default function App(): JSX.Element {
+  const { state, dispatch } = useContext(Store)
+
+  useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+
+  const toggleFavAction = (episode: IEpisode): IAction => {
+    const episodeInFav = state.favorites.includes(episode)
+    let dispatchObj = {
+      type: 'ADD_FAV',
+      payload: episode
+    }
+
+    if (episodeInFav) {
+      const favWithoutEpisode = state.favorites.filter(
+        (fav: IEpisode) => fav.id !== episode.id
+      )
+      dispatchObj = {
+        type: 'REMOVE_FAV',
+        payload: favWithoutEpisode
+      }
+    }
+    return dispatch(dispatchObj)
+  }
   return (
-    <React.Fragment>
-      <h1>Rick and Morty</h1>
-      <p>Pick your favorite episode</p>
-    </React.Fragment>
+    <Fragment>
+      <header className='header'>
+        <div>
+          <h1>Rick and Morty</h1>
+          <p>Pick your favorite episode</p>
+        </div>
+        <div>Favorite(s) : {state.favorites.length}</div>
+      </header>
+      <section className='episode-layout'>
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id} className='episode-box'>
+              <img
+                src={episode.image.medium}
+                alt={`Rick and Morty ${episode.name}`}
+              />
+              <div>{episode.name}</div>
+              <section>
+                <div>
+                  Season: {episode.season} Number: {episode.number}
+                </div>
+                <button type='button' onClick={() => toggleFavAction(episode)}>
+                  {state.favorites.find(
+                    (fav: IEpisode) => fav.id === episode.id
+                  )
+                    ? 'Unfav'
+                    : 'Fav'}
+                </button>
+              </section>
+            </section>
+          )
+        })}
+      </section>
+    </Fragment>
   )
 }
-
-export default App
---
+++ src/Store.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { IState, IAction } from './interfaces'
+
+const INITIAL_STATE: IState = {
+  episodes: [],
+  favorites: []
+}
+
+export const Store = React.createContext<IState | any>(INITIAL_STATE)
+
+function reducer(state: IState, action: IAction): IState {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      return { ...state, episodes: action.payload }
+    case 'ADD_FAV':
+      return { ...state, favorites: [...state.favorites, action.payload] }
+    case 'REMOVE_FAV':
+      return { ...state, favorites: action.payload }
+    default:
+      return state
+  }
+}
+
+export function StoreProvider(props: any): JSX.Element {
+  const [state, dispatch] = React.useReducer(reducer, INITIAL_STATE)
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  )
+}
