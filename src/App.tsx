df9d7219890f2d8b298d20afb398901bc40dfcb0
Vinicius <wh0adscrypt@gmail.com>
2020-01-24 10:56:59 -0300

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favorite episode</p>
+    </React.Fragment>
+  )
+}
+
+export default App
